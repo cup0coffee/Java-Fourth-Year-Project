edfde74c3902dbb1d8c8ba93c0437d8c6ecba482
Sean <seanmurchison@cmail.carleton.ca>
2022-10-05 16:47:31 -0400

    TEST - line 46: roll 1 skull, 4 parrots, 3 swords, hold parrots, reroll 3 swords, get 2 skulls 1 sword  die

--- src/test/java/PiratesGameTest.java
+++ src/test/java/PiratesGameTest.java
@@ -4,6 +4,7 @@ class PiratesGameTest {
 
     PiratesGame piratesGame;
     String[] dice;
+    String[] dieToKeep;
 
     @BeforeEach
     void init() {
@@ -35,4 +36,44 @@ class PiratesGameTest {
         Assertions.assertEquals(true, piratesGame.isPlayerDead(piratesGame.checkSkullCount(dice)));
 
     }
+
+    @Test
+    @DisplayName("line 46: roll 1 skull, 4 parrots, 3 swords, hold parrots, reroll 3 swords, get 2 skulls 1 sword  die")
+    void line46() {
+
+        //1. ROLL DICE
+        String[] dice = piratesGame.rollDice();
+
+        //2. HARD CODE DICE
+        dice[0] = "Skull";
+        dice[1] = "Parrot";
+        dice[2] = "Parrot";
+        dice[3] = "Parrot";
+        dice[4] = "Parrot";
+        dice[5] = "Sword";
+        dice[6] = "Sword";
+        dice[7] = "Sword";
+
+
+        //ASSERT THEY ARE NOT DEAD UNLESS THEY HAVE 3 OR MORE SKULLS (AND NOT FOUR)
+        Assertions.assertEquals(false, piratesGame.isPlayerDead(piratesGame.checkSkullCount(dice)));
+
+        //3. REROLL DICE THAT WERE SPECIFIED TO REROLL
+        dieToKeep = new String[]{"1", "2", "3", "4", "5"};
+        dice = piratesGame.reRollNotHeld(dice, dieToKeep);
+
+        //4. HARDCODE VALUES AGAIN
+        dice[0] = "Skull";
+        dice[1] = "Parrot";
+        dice[2] = "Parrot";
+        dice[3] = "Parrot";
+        dice[4] = "Parrot";
+        dice[5] = "Sword";
+        dice[6] = "Skull";
+        dice[7] = "Skull";
+
+        //ASSERT DEAD IF 3 SKULLS
+        Assertions.assertEquals(true, piratesGame.isPlayerDead(piratesGame.checkSkullCount(dice)));
+    }
+
 }
