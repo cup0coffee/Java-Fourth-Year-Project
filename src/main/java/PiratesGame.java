82d64d5e4092e6f47f366832dfa91eda2bf356e6
Sean <seanmurchison@cmail.carleton.ca>
2022-10-05 16:44:16 -0400

    CODE - line 45: die with 3 skulls on first roll

--- src/main/java/PiratesGame.java
+++ src/main/java/PiratesGame.java
@@ -6,4 +6,50 @@ public class PiratesGame implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    public String[] rollDice() {
+        String[] dieChoices = new String[6];
+
+        dieChoices[0] = ("Monkey");
+        dieChoices[1] = ("Parrot");
+        dieChoices[2] = ("Coin");
+        dieChoices[3] = ("Diamond");
+        dieChoices[4] = ("Sword");
+        dieChoices[5] = ("Skull");
+
+        String[] die = new String[8];
+
+        for (int i = 0; i < 8; i++) {
+            int rand = (int) (Math.random() * 6);
+            die[i] = dieChoices[rand];
+        }
+        return die;
+    }
+
+    public boolean isPlayerDead(int skullCount) {
+
+        boolean isDead = false;
+
+        if (skullCount >= 3 && skullCount != 4) {
+            isDead = true;
+        }
+
+        return isDead;
+    }
+
+    public int checkSkullCount(String[] dieRoll) {
+
+        int skull = 0;
+        int numOfDie = dieRoll.length;
+
+        for(int i =0; i < numOfDie; i++) {
+            if (dieRoll[i].equalsIgnoreCase("Skull")) {
+                skull++;
+            }
+        }
+
+        return skull;
+
+
+    }
+
 }
