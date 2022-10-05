7341adebde2e8a7b5009b8ab032fd4e948e9a627
Sean <seanmurchison@cmail.carleton.ca>
2022-10-05 19:10:32 -0400

    CODE - line 50: roll 1 skull, 2 parrots, 3 swords, 2 coins, reroll parrots get 2 coins, reroll 3 swords, get 3 coins (SC 4000 for seq of 8 (with FC) + 8x100=800 = 4800)

--- src/main/java/PiratesGame.java
+++ src/main/java/PiratesGame.java
@@ -60,6 +60,73 @@ public class PiratesGame implements Serializable {
         return dieRoll;
     }
 
+    public int scoreDie(String[] dieRoll, PiratesFortuneCard fortuneCard) {
+
+        int monkey = 0;
+        int parrot = 0;
+        int sword = 0;
+        int skull = 0;
+        int coin = 0;
+        int diamond = 0;
+
+        int numOfEach[] = new int[6];
+
+        int numOfDie = dieRoll.length;
+
+        for(int i = 0; i < numOfDie; i++) {
+            if (dieRoll[i].equalsIgnoreCase("Monkey")) {
+                monkey++;
+            } else if (dieRoll[i].equalsIgnoreCase("Parrot")) {
+                parrot++;
+            } else if (dieRoll[i].equalsIgnoreCase("Sword")) {
+                sword++;
+            } else if (dieRoll[i].equalsIgnoreCase("Skull")) {
+                skull++;
+            } else if (dieRoll[i].equalsIgnoreCase("Coin")) {
+                coin++;
+            } else if (dieRoll[i].equalsIgnoreCase("Diamond")) {
+                diamond++;
+            }
+        }
+
+        if(fortuneCard.getName().equalsIgnoreCase("Gold")) {
+            coin++;
+        }
+
+        numOfEach[0] = monkey;
+        numOfEach[1] = parrot;
+        numOfEach[2] = sword;
+        numOfEach[3] = skull;
+        numOfEach[4] = coin;
+        numOfEach[5] = diamond;
+
+        //CALCULATE SCORE
+        int score = 0;
+        int fullChestBonus = 0;
+
+        //ACCOUNT FOR IDENTICAL DIE
+        for (int i = 0; i < 6; i++) {
+            if (numOfEach[i] == 3) {
+                score+=100;
+            } else if (numOfEach[i] == 4) {
+                score+=200;
+            } else if (numOfEach[i] == 5) {
+                score+=500;
+            } else if (numOfEach[i] == 6) {
+                score+=1000;
+            } else if (numOfEach[i] == 7) {
+                score+=2000;
+            } else if (numOfEach[i] == 8) {
+                score+=4000;
+            }
+        }
+
+        //ACCOUNT FOR COINS
+        score += (100 * coin);
+
+        return score;
+    }
+
 
 
     public boolean isPlayerDead(int skullCount) {
