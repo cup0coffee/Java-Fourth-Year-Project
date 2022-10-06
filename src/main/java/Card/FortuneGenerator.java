1dce1d8da6504c3f6220f573cad340bcbd88a764
Sean <seanmurchison@cmail.carleton.ca>
2022-10-06 19:24:58 -0400

    CODE - line 54: score 2 sets of 3 (monkey, swords) in RTS on first roll   (SC 300)

--- src/main/java/Card/FortuneGenerator.java
+++ src/main/java/Card/FortuneGenerator.java
@@ -7,8 +7,12 @@ public class FortuneGenerator extends PiratesFortuneCardGenerator {
     PiratesFortuneCard createCard(int fortuneCardID) {
         if (fortuneCardID == 1) {
             return new Captain();
+
         } else if (fortuneCardID == 4) {
             return new Gold();
+
+        } else if (fortuneCardID == 6) {
+            return new MonkeyBusiness();
 
         } else {
             return null;
---
+++ src/main/java/Fortunes/MonkeyBusiness.java
@@ -0,0 +1,14 @@
+package Fortunes;
+
+import Card.PiratesFortuneCard;
+
+public class MonkeyBusiness extends PiratesFortuneCard {
+
+    public MonkeyBusiness() {
+        fortuneCardID = 6;
+        name = "Monkey Business";
+        description = "The Monkeys and the Parrots you roll are grouped together and are considered as one group for making a set. " +
+                "(Example: 2 Parrots and 3 Monkeys are considered as “five of a kind”)";
+    }
+
+}
