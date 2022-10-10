7ab53f3ab1c7ca9da69dd7f47aa55d0343287602
Sean <seanmurchison@cmail.carleton.ca>
2022-10-10 15:18:53 -0400

    REFACTOR - line 70: updated test to  specifications in v7 excel sheet

--- src/test/java/PiratesGameTest.java
+++ src/test/java/PiratesGameTest.java
@@ -779,7 +779,7 @@ class PiratesGameTest {
     }
 
     @Test
-    @DisplayName("line 70: score a set of 3 coins over 2 rolls  (SC 600)")
+    @DisplayName("line 70: roll 1 skull, 2 coins, 1 (monkey/parrot), 3 swords, reroll 3 swords, get 1 (coin/monkey/parrot)  (SC 600)")
     void line70() {
 
         //ALL THE FUNCTIONS IN THIS TEST ALREADY EXISTED FROM PREVIOUS CODE COMMITS
@@ -790,28 +790,28 @@ class PiratesGameTest {
 
         String[] dice = piratesGame.rollDice();
 
-        dice[0] = "Sword";
-        dice[1] = "Skull";
-        dice[2] = "Sword";
-        dice[3] = "Sword";
-        dice[4] = "Sword";
-        dice[5] = "Monkey";
-        dice[6] = "Monkey";
-        dice[7] = "Coin";
+        dice[0] = "Skull";
+        dice[1] = "Coin";
+        dice[2] = "Coin";
+        dice[3] = "Monkey";
+        dice[4] = "Parrot";
+        dice[5] = "Sword";
+        dice[6] = "Sword";
+        dice[7] = "Sword";
 
         Assertions.assertEquals(false, piratesGame.isPlayerDead(piratesGame.checkSkullCount(dice)));
 
-        dieToKeep = new String[]{"2", "8"};
+        dieToKeep = new String[]{"1", "2", "3", "4", "5"};
         dice = piratesGame.reRollNotHeld(dice, dieToKeep);
 
-        dice[0] = "Coin";
-        dice[1] = "Skull";
-        dice[2] = "Sword";
-        dice[3] = "Skull";
-        dice[4] = "Sword";
-        dice[5] = "Monkey";
-        dice[6] = "Coin";
-        dice[7] = "Coin";
+        dice[0] = "Skull";
+        dice[1] = "Coin";
+        dice[2] = "Coin";
+        dice[3] = "Monkey";
+        dice[4] = "Parrot";
+        dice[5] = "Coin";
+        dice[6] = "Monkey";
+        dice[7] = "Parrot";
 
         Assertions.assertEquals(false, piratesGame.isPlayerDead(piratesGame.checkSkullCount(dice)));
 
