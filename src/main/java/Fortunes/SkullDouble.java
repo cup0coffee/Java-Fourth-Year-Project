c7c4f211b7dec38efc05727a067597f156e9cf2b
Sean <seanmurchison@cmail.carleton.ca>
2022-10-13 09:54:53 -0400

    CODE - line 106: roll one skull and 7 swords with FC with two skulls => die

---
+++ src/main/java/Fortunes/SkullDouble.java
@@ -0,0 +1,14 @@
+package Fortunes;
+
+import Card.PiratesFortuneCard;
+
+public class SkullDouble extends PiratesFortuneCard {
+
+    public SkullDouble() {
+        fortuneCardID = 8;
+        name = "Two Skulls";
+        description = "You start your turn with the number of skulls that appear on the card.";
+    }
+
+}
+
